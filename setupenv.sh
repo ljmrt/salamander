03c7a45ec4b5401a6ec37d8b5c587710dd5572dd
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-04-19 19:38:41 -0600

    add .bashrc refresh to setupenv.sh
    source .bashrc to allow it to properly refresh after updating with new
    environment variable

--- setupenv.sh
+++ setupenv.sh
@@ -1,3 +1,4 @@
 SET_ROOT=`pwd`
 echo "export SALAMANDER_ROOT=$SET_ROOT" >> ~/.bashrc
 echo "Set %SALAMANDER_ROOT% environment variable to \"$SET_ROOT\"(appended to the end of .bashrc)."
+source ~/.bashrc
